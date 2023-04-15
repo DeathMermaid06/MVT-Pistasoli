e61c49e1ff370953dd3bcc2b665c461d28620cc9
Mariela Pistasoli <106203272+DeathMermaid06@users.noreply.github.com>
2023-04-15 19:09:28 -0300

    panel admin
    admin
    admin12345

--- MVT/AppMVT/admin.py
+++ MVT/AppMVT/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(cliente)
+admin.site.register(factura)
+admin.site.register(pedido)
 
 # Register your models here.
--- MVT/AppMVT/models.py
+++ MVT/AppMVT/models.py
@@ -5,16 +5,22 @@ class cliente(models.Model):
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     telefono=models.IntegerField()
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
 
 class pedido(models.Model):
     idCliente=models.IntegerField()
     sabor=models.CharField(max_length=50)
     cantidad=models.IntegerField()
     fecha=models.DateField()
+    def __str__(self):
+        return f"{self.sabor} - {self.cantidad}"
 
 class factura(models.Model):
     idCliente=models.IntegerField()
     monto=models.CharField(max_length=50)
     liquidado=models.BooleanField()
     fecha=models.DateField()
+    def __str__(self):
+        return f"{self.fecha} - ${self.monto}"
 
